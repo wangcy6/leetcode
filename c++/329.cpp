39c7e612ac8dccddff43d1cf7c1ec5b9241bf4a9
troy <wang_cyi@163.com>
2019-07-12 14:20:37 +0800

    1

--- c++/329.cpp
+++ c++/329.cpp
@@ -33,8 +33,10 @@ public:
     int row = matrix.size();
     if (!row)
       return 0;
+
     int col = matrix[0].size();
     vector<vector<int>> dp(row, vector<int>(col, 0));
+
     int maxd = 0, tmp;
     for (int i = 0; i < row; i++) {
       for (int j = 0; j < col; j++) {
@@ -54,11 +56,36 @@ int main(int argc, char *argv[]) {
   vector<int> a{9, 9, 4};
   vector<int> b{6, 6, 8};
   vector<int> c{2, 1, 1};
-
+/**
+00=1
+01=1
+12=1
+02=2
+10=2
+11=2
+20=3
+21=4
+22=2
+ */
   data.push_back(a);
   data.push_back(b);
   data.push_back(c);
+
+
+
   cout << test.longestIncreasingPath(data);
+
+    vector<vector<int>> data1;
+    vector<int> a1{3,4,5};
+    vector<int> b1{3, 2, 6};
+    vector<int> c1{2, 2, 1};
+
+    data1.push_back(a1);
+    data1.push_back(b1);
+    data1.push_back(c1);
+
+    cout << test.longestIncreasingPath(data1);
+
 
   return 0;
 }
