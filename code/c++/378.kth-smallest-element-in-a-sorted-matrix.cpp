9cc2792cdcb354ea08a589bac1a47205f45069fd
troy <wang_cyi@aliyun.com>
2019-08-13 15:06:33 +0800

    378

--- code/c++/378.kth-smallest-element-in-a-sorted-matrix.cpp
+++ code/c++/378.kth-smallest-element-in-a-sorted-matrix.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <queue>
 using namespace std; 
 
 //函数对象
@@ -13,15 +14,21 @@ public:
 
     }
     Item (int x, int y, int val) 
-    {
-        x = x;
-        y = y;
-        val = val;
+    {   
+        
+        this->x = x; //如果成员变量和参数相同，必须用this来区分标记
+        this->y = y;
+        this->val = val;
+
+        //cout<< "item():"<<this->x<<this->y <<this->val<<endl;
+
     }
-	//小顶堆是升序 
+    /**
+	//方法1 重载关系运算符
 	bool operator()(const Item  &first,const Item  &second) const{
 		return  first.val>second.val;
-	}
+	}**/
+    //方法2 重载比较运算符
 	bool operator<(const Item& b) const
    {
      return val > b.val;
@@ -37,12 +44,13 @@ public:
         int rows = arr.size();
 		if ( 0 == rows) return -1;
 		int cols = arr[0].size();
-        
-        priority_queue<Item,std::vector<Item>,Item()> min_heap;
+        priority_queue<Item> min_heap;
+       // priority_queue<Item,std::vector<Item>,Item> min_heap;
         
         for (int j=0;j<cols;j++)
 		{   
 	        Item item(0,j,arr[0][j]);
+           // cout<< "push" <<item.val<< ":"<<arr[0][j]<<endl;
 			min_heap.push(item);
 		}
         int result=0;
@@ -51,6 +59,7 @@ public:
 	        Item item = (Item)min_heap.top();
             min_heap.pop();
             result=item.val;
+            //cout<< result<<endl;
             if (item.x != rows-1)
             {  
                Item item1(item.x+1,item.y,arr[item.x+1][item.y]);
