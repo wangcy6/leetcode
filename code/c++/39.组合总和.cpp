7bc51038ec9182a96fbc20e63ce80d312cb7c205
troy <wang_cyi@163.com>
2019-12-01 08:42:52 +0800

    update

--- code/c++/39.组合总和.cpp
+++ code/c++/39.组合总和.cpp
@@ -47,6 +47,14 @@
  */
 
 // @lc code=start
+/*参考代码：https://leetcode-cn.com/problems/combination-sum/solution/hui-su-suan-fa-jian-zhi-python-dai-ma-java-dai-m-2/
+ https://leetcode-cn.com/problems/combination-sum/solution/hui-shuo-suan-fa-di-gui-java-by-longchenghuang/
+ 
+ 测试
+ 1. 加入输入一个元素a[1], k=100,输出 {100个1}，递归层次不是1  元素可重复选取。
+ 2.  candidates = [2,3,6,7], target = 7, 每次n=4全部计算一次。 集合特点是顺的。
+
+*/
 class Solution {
 public:
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
