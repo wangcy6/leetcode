a8db32bd23bbcb99670008f48f4ff1d7d0c5b41b
troy <wang_cyi@163.com>
2019-12-22 09:15:22 +0800

    update

---
+++ code/c++/65.有效数字.cpp
@@ -0,0 +1,43 @@
+/*
+ * @lc app=leetcode.cn id=65 lang=cpp
+ *
+ * [65] 有效数字
+ * 验证给定的字符串是否可以解释为十进制数字。
+ * https://leetcode-cn.com/problems/valid-number/solution/xiang-xi-tong-su-de-si-lu-fen-xi-duo-jie-fa-by-1-4/
+ 
+ * 
+ */
+
+/**
+ * 例如:
+
+"0" => true
+" 0.1 " => true
+"abc" => false
+"1 a" => false
+"2e10" => true
+" -90e3   " => true
+" 1e" => false
+"e3" => false
+" 6e-1" => true
+" 99e2.5 " => false
+"53.5e93" => true
+" --6 " => false
+"-+3" => false
+"95a54e53" => false
+ */
+// @lc code=start
+//https://github.com/fuwutu/LeetCode/blob/master/Valid%20Number.cpp
+//https://zhuanlan.zhihu.com/p/34639172
+//https://www.zhihu.com/question/31845498
+class Solution {
+public:
+    bool isNumber(string s) {
+
+        int length = s.size();
+
+        
+    }
+};
+// @lc code=end
+
