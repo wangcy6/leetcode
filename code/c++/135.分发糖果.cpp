2572aacdea9bbe0af9ff0ca2159cfa346fadd8e7
troy <wang_cyi@163.com>
2020-01-12 17:14:47 +0800

    update

--- code/c++/135.分发糖果.cpp
+++ code/c++/135.分发糖果.cpp
@@ -54,6 +54,39 @@ public:
       } 
        return sum;
     }
+
+    
+//https://www.nowcoder.com/questionTerminal/72015680c32b449899e81f1470836097
+int dp(int n)
+{
+    if( n ==1) return 1;
+    if (n ==2 ) return 2;
+    
+    return dp(n-1)+dp(n-2);
+}
+//递归改成非递归
+//  dp(n-1)+dp(n-2)这个不知道如何改写了。
+int eatCandy(int n)
+{  
+   if( n < 3)
+   {
+      return n ==2?2:1;
+   }
+   vector<int> dp(n+1,0); 
+
+   dp[1] =1;
+   dp[2] =2;
+
+   for( int i=3;i<=n;i++)
+   {
+     dp[i] =dp[i-1]+dp[i-2];
+   }
+
+   return dp[n];
+
+
+}
+
 };
 // @lc code=end
 
