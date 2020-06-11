783c9c86904f0a08d47ca4566c8d7953d595fe96
wang_cyi <wang_cyi@163.com>
2020-06-11 15:58:54 +0800

    update

---
+++ code/c++/378.有序矩阵中第k小的元素.cpp
@@ -0,0 +1,48 @@
+/*
+ * @lc app=leetcode.cn id=378 lang=cpp
+ *
+ * [378] 有序矩阵中第K小的元素
+ *
+ * https://leetcode-cn.com/problems/kth-smallest-element-in-a-sorted-matrix/description/
+ *
+ * algorithms
+ * Medium (59.11%)
+ * Likes:    232
+ * Dislikes: 0
+ * Total Accepted:    21.9K
+ * Total Submissions: 36.8K
+ * Testcase Example:  '[[1,5,9],[10,11,13],[12,13,15]]\n8'
+ *
+ * 给定一个 n x n 矩阵，其中每行和每列元素均按升序排序，找到矩阵中第k小的元素。
+ * 请注意，它是排序后的第 k 小元素，而不是第 k 个不同的元素。
+ * 
+ * 
+ * 
+ * 示例:
+ * 
+ * matrix = [
+ * ⁠  [ 1,  5,  9],
+ * ⁠  [10, 11, 13],
+ * ⁠  [12, 13, 15]
+ * ],
+ * k = 8,
+ * 
+ * 返回 13。
+ * 
+ * 
+ * 
+ * 
+ * 提示：
+ * 你可以假设 k 的值永远是有效的, 1 ≤ k ≤ n^2 。
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int kthSmallest(vector<vector<int>>& matrix, int k) {
+
+    }
+};
+// @lc code=end
+
