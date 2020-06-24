278e86b8a04d5af6efa9ecc4416b4981c38b7849
wang_cyi <wang_cyi@163.com>
2020-06-24 18:10:26 +0800

    update

---
+++ code/c++/76.最小覆盖子串.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=76 lang=cpp
+ *
+ * [76] 最小覆盖子串
+ *
+ * https://leetcode-cn.com/problems/minimum-window-substring/description/
+ *
+ * algorithms
+ * Hard (37.90%)
+ * Likes:    603
+ * Dislikes: 0
+ * Total Accepted:    58K
+ * Total Submissions: 152.7K
+ * Testcase Example:  '"ADOBECODEBANC"\n"ABC"'
+ *
+ * 给你一个字符串 S、一个字符串 T，请在字符串 S 里面找出：包含 T 所有字符的最小子串。
+ * 
+ * 示例：
+ * 
+ * 输入: S = "ADOBECODEBANC", T = "ABC"
+ * 输出: "BANC"
+ * 
+ * 说明：
+ * 
+ * 
+ * 如果 S 中不存这样的子串，则返回空字符串 ""。
+ * 如果 S 中存在这样的子串，我们保证它是唯一的答案。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        
+    }
+};
+// @lc code=end
+
