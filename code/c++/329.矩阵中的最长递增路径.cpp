5af90ef96a923f1b188d4de9eed17f74c3552452
wang_cyi <wang_cyi@163.com>
2020-07-14 13:56:49 +0800

    update

--- code/c++/329.矩阵中的最长递增路径.cpp
+++ code/c++/329.矩阵中的最长递增路径.cpp
@@ -15,9 +15,11 @@
  *
  * 给定一个整数矩阵，找出最长递增路径的长度。
  *
- * 对于每个单元格，你可以往上，下，左，右四个方向移动。 你不能在对角线方向上移动或移动到边界外（即不允许环绕）。
+ * 对于每个单元格，你可以往上，下，左，右四个方向移动。
+ *  你不能在对角线方向上移动或移动到边界外（即不允许环绕）。
+
+ 
  * 
- *
  * 示例 1:
  *
  * 输入: nums = 
@@ -50,9 +52,11 @@
  */
 
 // @lc code=start
-class Solution {
+class Solution
+{
 public:
-  int longestIncreasingPath(vector<vector<int>> &matrix) {
+  int longestIncreasingPath(vector<vector<int>> &matrix)
+  {
     int row = matrix.size();
     if (0 == row)
       return 0;
@@ -60,13 +64,13 @@ public:
 
     vector<vector<int>> dp(row, vector(col, 1)); //记录从每个点开始最长的路径
     int path = 0;
-    for (int i = 0; i < row; i++) {
-      for (int j = 0; j < col; j++) {
+    for (int i = 0; i < row; i++)
+    {
+      for (int j = 0; j < col; j++)
+      {
         //任意坐标开始都有可能
-        
-          int temp = dfs(matrix, dp, i, j, INT_MIN);
-          path = max(temp, path);
-        
+        int temp = dfs(matrix, dp, i, j, INT_MIN);
+        path = max(temp, path);
       }
     }
 
@@ -74,15 +78,19 @@ public:
   }
   // row ，cols 开始坐标位置
   int dfs(vector<vector<int>> &matrix, vector<vector<int>> &dp, int row,
-          int col, int pre) {
-    if (row < 0 || col < 0 || row >= matrix.size() || col >= matrix[0].size()) {
+          int col, int pre)
+  {
+    if (row < 0 || col < 0 || row >= matrix.size() || col >= matrix[0].size())
+    {
       return 0; // 最简单的case
     }
-    if (pre >= matrix[row][col]) {
+    if (pre >= matrix[row][col])
+    {
       return 0; //不是递增了，肯定是0
     }
 
-    if (dp[row][col] > 1) {
+    if (dp[row][col] > 1)
+    {
       return dp[row][col]; //避免重复计算
     }
 
@@ -91,8 +99,8 @@ public:
     int up = dfs(matrix, dp, row - 1, col, matrix[row][col]);
     int down = dfs(matrix, dp, row + 1, col, matrix[row][col]);
 
-      dp[row][col] = max(max(left, right), max(up, down)) + 1; //变化地方
-      return dp[row][col];
+    dp[row][col] = max(max(left, right), max(up, down)) + 1; //变化地方
+    return dp[row][col];
   }
 };
 // @lc code=end
--- code/c++/365.水壶问题.cpp
+++ code/c++/365.水壶问题.cpp
@@ -31,16 +31,114 @@
  * 输入: x = 2, y = 6, z = 5
  * 输出: False
  * 
- * 
+ *  题目 不理解里面含义
  */
 
 // @lc code=start
+
 class Solution
 {
 public:
     bool canMeasureWater(int x, int y, int z)
     {
-        //
+        if (z < 0 || z > x + y)
+            return false;
+
+        std::set<int> s;
+        std::queue<int> q({0});
+
+        while (q.size() > 0)
+        {
+            int n = q.front();
+            if (n + x <= x + y && true == s.insert(n + x).second)
+                q.push(n + x);
+            if (n + y <= x + y && true == s.insert(n + y).second)
+                q.push(n + y);
+            if (n - x >= 0 && true == s.insert(n - x).second)
+                q.push(n - x);
+            if (n - y >= 0 && true == s.insert(n - y).second)
+                q.push(n - y);
+            if (s.end() != s.find(z))
+                return true;
+
+            q.pop();
+        }
+
+        return false;
     }
 };
+
+class Solution1
+{
+    inline pair<int, int> op(int type, const pair<int, int> &state, int x, int y)
+    {
+        switch (type)
+        {
+        case 0:
+            return make_pair(x, state.second);
+        case 1:
+            return make_pair(state.first, y);
+        case 2:
+            return make_pair(0, state.second);
+        case 3:
+            return make_pair(state.first, 0);
+        case 4:
+        {
+            int move = min(state.first, y - state.second);
+            return make_pair(state.first - move, state.second + move);
+        }
+        case 5:
+        {
+            int move = min(x - state.first, state.second);
+            return make_pair(state.first + move, state.second - move);
+        }
+        }
+        return make_pair(0, 0);
+    }
+    struct HashPair
+    {
+        size_t operator()(const pair<int, int> &key) const noexcept
+        {
+            return (size_t(key.first) << 31) | size_t(key.second);
+        }
+    };
+    inline int64_t Hash(int x, int y)
+    {
+        return int64_t(x) << 32 | y;
+    }
+
+public:
+    bool canMeasureWater(int x, int y, int z)
+    {
+        if (x + y < z)
+        { //加了一个很蠢的剪枝，作用比较大。
+            return false;
+        }
+        unordered_set<int64_t> mark; //pair<int, int> 换成int64_t, 但是意义不大。
+        queue<pair<int, int>> q;
+        q.push(make_pair(0, 0));
+        while (q.empty() == false)
+        {
+            auto f = q.front();
+            q.pop();
+            if (f.first + f.second == z)
+            {
+                return true;
+            }
+            for (int i = 0; i < 6; i++)
+            {
+                auto next = op(i, f, x, y);
+                int64_t h = Hash(next.first, next.second);
+                if (mark.find(h) != mark.end())
+                {
+                    continue;
+                }
+                mark.insert(h);
+                q.push(next);
+            }
+        }
+        return false;
+    }
+};
+
 // @lc code=end
