afe96721f5f31536910c5f51699f6263303e7e70
troy <wang_cyi@163.com>
2020-07-21 05:19:27 +0800

    update

---
+++ study/c++_study/cpp/2020/code/3_core.cpp
@@ -0,0 +1,19 @@
+<<<<<<< HEAD
+#include <stdio.h>
+int main()
+{
+       int *p = NULL;
+       *p = 0;
+
+       return 0;
+}
+=======
+#include <stdio.h>
+int main()
+{
+       int *p = NULL;
+       *p = 0;
+
+       return 0;
+}
+>>>>>>> 55d65b891bace52a834abb8e74e3ab2ec75d63f6
