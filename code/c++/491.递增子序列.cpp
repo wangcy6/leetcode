90f307f8d3b5bbe15c808e1ebbcaad6cb527b4eb
wang_cyi <wang_cyi@163.com>
2020-08-25 13:57:10 +0800

    update

---
+++ code/c++/491.递增子序列.cpp
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=491 lang=cpp
+ *
+ * [491] 递增子序列
+ *
+ * https://leetcode-cn.com/problems/increasing-subsequences/description/
+ *
+ * algorithms
+ * Medium (48.93%)
+ * Likes:    127
+ * Dislikes: 0
+ * Total Accepted:    12.6K
+ * Total Submissions: 24.2K
+ * Testcase Example:  '[4,6,7,7]'
+ *
+ * 给定一个整型数组, 你的任务是找到所有该数组的递增子序列，递增子序列的长度至少是2。
+ * 
+ * 示例:
+ * 
+ * 
+ * 输入: [4, 6, 7, 7]
+ * 输出: [[4, 6], [4, 7], [4, 6, 7], [4, 6, 7, 7], [6, 7], [6, 7, 7], [7,7],
+ * [4,7,7]]
+ * 
+ * 说明:
+ * 
+ * 
+ * 给定数组的长度不会超过15。
+ * 数组中的整数范围是 [-100,100]。
+ * 给定数组中可能包含重复数字，相等的数字应该被视为递增的一种情况。
+ * 
+ * 
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<vector<int>> findSubsequences(vector<int>& nums)
+    {
+    }
+};
+// @lc code=end
