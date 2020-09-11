4a6f81afa178be71f2cee3b9732a31f3ca46bdc3
wang_cyi <wang_cyi@163.com>
2020-09-11 13:56:47 +0800

    update

--- code/c++/216.组合总和-iii.cpp
+++ code/c++/216.组合总和-iii.cpp
@@ -34,6 +34,7 @@
  * 输出: [[1,2,6], [1,3,5], [2,3,4]]
  * 
  * stack-overflow on address 0x7ffe5f4e0ff8 
+ * 解集不能包含重复的组合 
  */
 
 // @lc code=start
@@ -43,15 +44,21 @@ public:
      *   tree最多分支：9
      *   tree的高度：约束累计和为n path
      *   遍历：递归回溯，只要求计算k个。元素
+     *   逻辑视图：索引代表 tree的关系， 
+     *   二叉树： root  root->left 2层
+     *   堆：i 2i+1 代表上下层关系：
+     *   这里 i，i+1 代表上下层关系 
      *   
-     * 
+     *  线索：解集不能包含重复的组合。
+     *  约束：不会无限递归
+     *  逻辑：数字 i 和i代表 2个层次。
      */
     vector<vector<int>> combinationSum3(int k, int n)
     {
 
         vector<vector<int>> result; //返回结果
         vector<int> path; //保存整个路径
-        int start = 1; //访问标记
+        int level = 1; //访问标记
         dfs(n, start, k, path, result);
 
         return result;
@@ -59,7 +66,9 @@ public:
     void dfs(int sum, int start, int k, vector<int>& path, vector<vector<int>>& result)
     {
 
-        //虽然不要求获取全部，但是依然全部遍历
+        //case1 假如 sum  不是一致递归循环吗？
+        //不会 因为 第一次10个选择，第二次 9个，最后一次 1个，每次递归i=start开始，不是0开始。
+        //解集不能包含重复的组合只重要线索
         if (sum == 0 && k == path.size()) {
             result.push_back(path);
             return;
