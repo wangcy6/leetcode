843382e0714aae822d02696cad13e21c4f39527e
wang_cyi <wang_cyi@163.com>
2020-10-17 09:31:17 +0800

    update

--- study/go_study/sdk/utl/string/test_string.go
+++ study/go_study/sdk/utl/string/test_string.go
@@ -2,16 +2,24 @@ package main
 
 import "fmt"
 
+type NullString struct {
+	String string
+	Valid  bool // Valid is true if String is not NULL
+}
+
 func main() {
 
-	data := []interface{nil, 0}
+	//	data := []interface{nil, 0}
 
-	if str, ok := data.(string); ok {
-		/* act on str */
-		fmt.Println("ok", str)
-	} else {
-		/* not string */
-		fmt.Println("err", str)
-	}
+	//	if str, ok := data.(string); ok {
+	//		/* act on str */
+	//		fmt.Println("ok", str)
+	//	} else {
+	//		/* not string */
+	//		fmt.Println("err", str)
+	//	}
+	input := "helloworld"
+	test := NullString{input, true}
+	fmt.Println("data", test.String)
 
 }
