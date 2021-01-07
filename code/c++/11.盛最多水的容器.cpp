e892dafa6a81911d53b6c0af155aa2e356b739c3
wang_cyi <wang_cyi@163.com>
2021-01-07 15:59:00 +0800

    update

--- code/c++/11.盛最多水的容器.cpp
+++ code/c++/11.盛最多水的容器.cpp
@@ -67,6 +67,20 @@ class Solution {
 public:
     int maxArea(vector<int>& height)
     {
+        int left = 0;
+        int right = height.size() - 1;
+        int area = 0;
+        while (left < right) {
+            if (height[left] < height[right]) {
+                area = max(area, height[left] * (right - left));
+                left++;
+            } else {
+                area = max(area, height[right] * (right - left));
+                right--;
+            }
         }
+
+        return area;
+    }
 };
 // @lc code=end
