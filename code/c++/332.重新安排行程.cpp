717e13a01e279f3f7bc2c01534fa0699ef219117
wang_cyi <wang_cyi@163.com>
2021-01-14 13:56:52 +0800

    update

--- code/c++/332.重新安排行程.cpp
+++ code/c++/332.重新安排行程.cpp
@@ -14,13 +14,15 @@
  * Testcase Example:  '[["MUC","LHR"],["JFK","MUC"],["SFO","SJC"],["LHR","SFO"]]'
  *
  * 给定一个机票的字符串二维数组 [from,
- * to]，子数组中的两个成员分别表示飞机出发和降落的机场地点，对该行程进行重新规划排序。所有这些机票都属于一个从
+ * to]，子数组中的两个成员分别表示飞机出发和降落的机场地点，对该行程进行重新规划排序。
+ * 所有这些机票都属于一个从
  * JFK（肯尼迪国际机场）出发的先生，所以该行程必须从 JFK 开始。
  * 
  * 说明:
  * 
  * 
- * 如果存在多种有效的行程，你可以按字符自然排序返回最小的行程组合。例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"]
+ * 如果存在多种有效的行程，你可以按字符自然排序返回最小的行程组合。
+ * 例如，行程 ["JFK", "LGA"] 与 ["JFK", "LGB"]
  * 相比就更小，排序更靠前
  * 所有的机场都用三个大写字母表示（机场代码）。
  * 假定所有机票至少存在一种合理的行程。
@@ -36,16 +38,17 @@
  * 
  * 输入: [["JFK","SFO"],["JFK","ATL"],["SFO","ATL"],["ATL","JFK"],["ATL","SFO"]]
  * 输出: ["JFK","ATL","JFK","SFO","ATL","SFO"]
- * 解释: 另一种有效的行程是 ["JFK","SFO","ATL","JFK","ATL","SFO"]。但是它自然排序更大更靠后。
+ * 
+ * 解释: 另一种有效的行程是 ["JFK","SFO","ATL","JFK","ATL","SFO"]。
+ * 但是它自然排序更大更靠后。
  * 
  */
 
 // @lc code=start
 class Solution {
 public:
-    vector<string> findItinerary(vector<vector<string>>& tickets) {
-
+    vector<string> findItinerary(vector<vector<string>>& tickets)
+    {
     }
 };
 // @lc code=end
-
